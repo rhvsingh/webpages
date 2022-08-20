b9339cbf972b840a3ffd076fe174901a26335109
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-08-20 21:04:46 +0530

    Changes made in pageData for images

--- js/pageData.js
+++ js/pageData.js
@@ -1,60 +1,60 @@
 const pageData = [{
     link:'webpage_10',
-    imageLink: 'Clone_of_Bootstrap_Template.png',
+    imageLink: 'Clone_of_Bootstrap_Template.webp',
     altImage: 'Clone of Bootstrap Template',
     title: 'Clone of Bootstrap Template',
     date: '02/01/2022'
 },{
     link:'webpage_9',
-    imageLink: 'Clone_of_Layout_Parser.png',
+    imageLink: 'Clone_of_Layout_Parser.webp',
     altImage: 'Clone of Layout Parser',
     title: 'Clone of Layout Parser',
     date: '01/28/2022'
 },{
     link:'https://rakshit234.github.io/Portfolio',
-    imageLink: 'Rakshit_Bhaiya_Portfolio.png',
+    imageLink: 'Rakshit_Bhaiya_Portfolio.webp',
     altImage: 'Rakshit Bhaiya Portfolio',
     title: 'Rakshit Bhaiya Portfolio',
     date: '12/01/2021'
 },{
     link:'webpage_7',
-    imageLink: 'Hulu_Clone_Project.png',
+    imageLink: 'Hulu_Clone_Project.webp',
     altImage: 'Hulu Clone Project',
     title: 'Hulu Clone Project',
     date: '10/13/2021'
 },{
     link:'webpage_6',
-    imageLink: 'HTML5_Massively_Landing_Page.png',
+    imageLink: 'HTML5_Massively_Landing_Page.webp',
     altImage: 'HTML5 Massively Landing Page',
     title: 'HTML5 Massively Landing Page',
     date: '10/11/2021'
 },{
     link:'webpage_5',
-    imageLink: 'Eventually_Stunning_HTML5_Landing_Page.png',
+    imageLink: 'Eventually_Stunning_HTML5_Landing_Page.webp',
     altImage: 'Eventually - Stunning HTML5 Landing Page',
     title: 'Eventually - Stunning HTML5 Landing Page',
     date: '10/09/2021'
 },{
     link:'webpage_4',
-    imageLink: 'Slack_Clone_Project.png',
+    imageLink: 'Slack_Clone_Project.webp',
     altImage: 'Slack Clone Project',
     title: 'Slack Clone Project',
     date: '10/07/2021'
 },{
     link:'webpage_3',
-    imageLink: 'Multiverse_HTML5_Photo_Gallery_Project.png',
+    imageLink: 'Multiverse_HTML5_Photo_Gallery_Project.webp',
     altImage: 'Multiverse HTML5 Photo Gallery Project',
     title: 'Multiverse HTML5 Photo Gallery Project',
     date: '10/05/2021'
 },{
     link:'webpage_2',
-    imageLink: 'Editorial_HTML5.png',
+    imageLink: 'Editorial_HTML5.webp',
     altImage: 'Editorial HTML5 Blog Project',
     title: 'Editorial HTML5 Blog Project',
     date: '10/03/2021'
 },{
     link:'webpage_1',
-    imageLink: 'Astral_Portfolio.png',
+    imageLink: 'Astral_Portfolio.webp',
     altImage: 'Astral Portfolio Page',
     title: 'Astral Portfolio Page',
     date: '10/01/2021'
