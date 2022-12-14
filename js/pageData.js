035f7dbf6e946c2061ff86cd37a99064a9785e80
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-12-14 22:35:01 +0530

    feat: added description section and css for it and soem transition delays

--- js/pageData.js
+++ js/pageData.js
@@ -1,67 +1,85 @@
 const pageData = [{
+    link:'https://rhvsingh.github.io/intern-putatoe',
+    imageLink: 'Intern-Putatoe.webp',
+    altImage: 'Intern Putatoe',
+    title: 'Intern Putatoe ReactJS',
+    desc: 'Clone of Intern Putatoe App Homepage using ReactJS. Highly composible and flexible app.',
+    date: '11/15/2022'
+},{
     link:'webpage_11',
     imageLink: 'Celebrare_Assignment.webp',
     altImage: 'Celebrare Assignment',
     title: 'Celebrare Assignment Image',
+    desc: '',
     date: '11/19/2022'
 },{
     link:'webpage_10',
     imageLink: 'Clone_of_Bootstrap_Template.webp',
     altImage: 'Clone of Bootstrap Template',
     title: 'Clone of Bootstrap Template',
+    desc: '',
     date: '02/01/2022'
 },{
     link:'webpage_9',
     imageLink: 'Clone_of_Layout_Parser.webp',
     altImage: 'Clone of Layout Parser',
     title: 'Clone of Layout Parser',
+    desc: '',
     date: '01/28/2022'
 },{
     link:'https://rakshit234.github.io/Portfolio',
     imageLink: 'Rakshit_Bhaiya_Portfolio.webp',
     altImage: 'Portfolio',
     title: 'Portfolio',
+    desc: '',
     date: '12/01/2021'
 },{
     link:'webpage_7',
     imageLink: 'Hulu_Clone_Project.webp',
     altImage: 'Hulu Clone Project',
     title: 'Hulu Clone Project',
+    desc: '',
     date: '10/13/2021'
 },{
     link:'webpage_6',
     imageLink: 'HTML5_Massively_Landing_Page.webp',
     altImage: 'HTML5 Massively Landing Page',
     title: 'HTML5 Massively Landing Page',
+    desc: '',
     date: '10/11/2021'
 },{
     link:'webpage_5',
     imageLink: 'Eventually_Stunning_HTML5_Landing_Page.webp',
     altImage: 'Eventually - Stunning HTML5 Landing Page',
     title: 'Eventually - Stunning HTML5 Landing Page',
+    desc: '',
     date: '10/09/2021'
 },{
     link:'webpage_4',
     imageLink: 'Slack_Clone_Project.webp',
     altImage: 'Slack Clone Project',
     title: 'Slack Clone Project',
+    desc: '',
     date: '10/07/2021'
 },{
     link:'webpage_3',
     imageLink: 'Multiverse_HTML5_Photo_Gallery_Project.webp',
     altImage: 'Multiverse HTML5 Photo Gallery Project',
     title: 'Multiverse HTML5 Photo Gallery Project',
+    desc: '',
     date: '10/05/2021'
 },{
     link:'webpage_2',
     imageLink: 'Editorial_HTML5.webp',
     altImage: 'Editorial HTML5 Blog Project',
     title: 'Editorial HTML5 Blog Project',
+    desc: '',
     date: '10/03/2021'
 },{
     link:'webpage_1',
     imageLink: 'Astral_Portfolio.webp',
     altImage: 'Astral Portfolio Page',
     title: 'Astral Portfolio Page',
+    desc: '',
     date: '10/01/2021'
 }]
--- js/script.js
+++ js/script.js
@@ -14,14 +14,36 @@ let numCheck = true
 let completePage
 
 function dataInserter(data) {
-    let convertedData = `<article>
-                            <a href="${data.link}/">
-                                <div class="image-container">
-                                    <img src="images/${data.imageLink}" alt="${data.altImage}" />
-                                </div>
-                                <div class="content-container">${data.title}</div>
-                            </a>
-                        </article>`
+
+    let convertedData
+    if (data.desc == '') {
+
+        convertedData =
+            `<article>
+                <a href="${data.link}/">
+                    <div class="image-container">
+                        <img src="images/${data.imageLink}" alt="${data.altImage}" />
+                    </div>
+                    <div class="content-container">
+                        <div class="content-title">${data.title}</div>
+                        <div class="content-description"></div>
+                    </div>
+                </a>
+            </article>`
+    } else {
+        convertedData = 
+        `<article>
+            <a href="${data.link}/">
+                <div class="image-container">
+                    <img src="images/${data.imageLink}" alt="${data.altImage}" />
+                </div>
+                <div class="content-container data">
+                    <div class="content-title">${data.title}</div>
+                    <div class="content-description">${data.desc}</div>
+                </div>
+            </a>
+        </article>`
+    }
     content.innerHTML += convertedData
 }
 
@@ -62,7 +84,7 @@ function changeView(view) {
 
     console.log(currentPage + ' ' + completePage)
 
-    if(!numCheck) {
+    if (!numCheck) {
         if (currentPage > completePage) {
             nextButton.style.visibility = 'hidden'
             nextButton.style.opacity = 0
@@ -75,10 +97,10 @@ function changeView(view) {
         if (currentPage < completePage) {
             nextButton.style.visibility = 'visible'
             nextButton.style.opacity = 1
-            
+
         } else {
             console.log('Do nothing')
-            
+
             nextButton.style.visibility = 'hidden'
             nextButton.style.opacity = 0
         }
@@ -164,7 +186,7 @@ if (pageData.length / counter && pageData.length % counter == 0) {
         }
         paginationCount.innerHTML += `<span class='post-button' onclick='changeView(${i})' data-id='${i}'>${i}</span>`
     }
-    paginationCount.innerHTML += `<span class='post-button' onclick='changeView(${completePage+1})' data-id='${completePage+1}'>${completePage+1}</span>`
+    paginationCount.innerHTML += `<span class='post-button' onclick='changeView(${completePage + 1})' data-id='${completePage + 1}'>${completePage + 1}</span>`
 }
 
 prevButton.style.visibility = 'hidden'
@@ -183,10 +205,10 @@ let onChangeChecker = 0
 
 dropOptions[0].classList.add('active-drop')
 showSelectedDrop.innerHTML = dropOptions[0].innerHTML
-showSelectedDrop.setAttribute('value',dropOptions[0].getAttribute('value'))
+showSelectedDrop.setAttribute('value', dropOptions[0].getAttribute('value'))
 
 dropDown.onclick = (e) => {
-    if(dropDown.classList.toggle('active-drop')){
+    if (dropDown.classList.toggle('active-drop')) {
         console.log('add')
     } else {
         console.log('remove')
@@ -199,42 +221,42 @@ dropDown.onclick = (e) => {
 //dropDown.addEventListener('click', identify, false);
 
 function identify(e) {
-  for (let i = 0; i < dropOptions.length; i++) {
-    if (dropOptions[i] === e.target) {
-        if (onChangeChecker == i) {
-            continue
-        } else {
-            onChangeChecker = i
-            console.log(i);
-            showSelectedDrop.innerHTML = dropOptions[i].innerHTML
-            showSelectedDrop.setAttribute('value',dropOptions[i].getAttribute('value'))
-            switch (e.target.getAttribute('value')) {
-                case 'recent':
-                    clickChange = 0
-                    content.innerHTML = ''
-                    /* dataChange.forEach(dataInserter) */
-                    contentLoader(start, end, dataChange)
-                    break;
-                case 'old':
-                    set = dataChange.slice().sort((a, b) => {
-                        let date1 = new Date(a.date)
-                        let date2 = new Date(b.date)
-                        return date1 - date2
-                    })
-                    clickChange = 1
-                    content.innerHTML = ''
-                    /* set.forEach(dataInserter) */
-                    contentLoader(start, end, set)
-                    break;
+    for (let i = 0; i < dropOptions.length; i++) {
+        if (dropOptions[i] === e.target) {
+            if (onChangeChecker == i) {
+                continue
+            } else {
+                onChangeChecker = i
+                console.log(i);
+                showSelectedDrop.innerHTML = dropOptions[i].innerHTML
+                showSelectedDrop.setAttribute('value', dropOptions[i].getAttribute('value'))
+                switch (e.target.getAttribute('value')) {
+                    case 'recent':
+                        clickChange = 0
+                        content.innerHTML = ''
+                        /* dataChange.forEach(dataInserter) */
+                        contentLoader(start, end, dataChange)
+                        break;
+                    case 'old':
+                        set = dataChange.slice().sort((a, b) => {
+                            let date1 = new Date(a.date)
+                            let date2 = new Date(b.date)
+                            return date1 - date2
+                        })
+                        clickChange = 1
+                        content.innerHTML = ''
+                        /* set.forEach(dataInserter) */
+                        contentLoader(start, end, set)
+                        break;
+                }
             }
+
         }
-        
     }
-  }
 }
 
-dropToggle.addEventListener('mouseover', function(e){
-    for(let i = 0; i < dropOptions.length; i++) {
+dropToggle.addEventListener('mouseover', function (e) {
+    for (let i = 0; i < dropOptions.length; i++) {
         dropOptions[i].classList.remove('active-drop')
     }
     e.target.classList.add('active-drop')
