1698bcb25004ea11e0388419031bd1ade7c584bf
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2023-04-17 22:47:35 +0530

    feat: smooth transition of title on hover and added description of some projects

--- js/pageData.js
+++ js/pageData.js
@@ -1,120 +1,138 @@
-const pageData = [{
-    link:'https://first-react-app-rhvsingh.vercel.app/quizapp',
-    imageLink: 'Quiz_App.webp',
-    altImage: 'Quiz App',
-    title: 'Quiz App',
-    desc: '',
-    date: '12/03/2022'
-},{
-    link:'https://first-react-app-rhvsingh.vercel.app/tenzies',
-    imageLink: 'Tenzies_Game.webp',
-    altImage: 'Tenzies Game',
-    title: 'Tenzies Game',
-    desc: '',
-    date: '11/30/2022'
-},{
-    link:'https://first-react-app-rhvsingh.vercel.app/notes',
-    imageLink: 'Notes_App.webp',
-    altImage: 'Notes App',
-    title: 'Notes App',
-    desc: '',
-    date: '11/28/2022'
-},{
-    link:'https://first-react-app-rhvsingh.vercel.app/',
-    imageLink: 'Task_Tracker.webp',
-    altImage: 'Task Tracker',
-    title: 'Task Tracker',
-    desc: '',
-    date: '11/25/2022'
-},{
-    link:'https://rhvsingh.github.io/intern-putatoe',
-    imageLink: 'Intern-Putatoe.webp',
-    altImage: 'Intern Putatoe',
-    title: 'Intern Putatoe ReactJS',
-    desc: 'Clone of Intern Putatoe App Homepage using ReactJS. Highly composible and flexible app.',
-    date: '11/15/2022'
-},{
-    link:'webpage_11',
-    imageLink: 'Celebrare_Assignment.webp',
-    altImage: 'Celebrare Assignment',
-    title: 'Celebrare Assignment Image',
-    desc: '',
-    date: '11/19/2022'
-},{
-    link:'webpage_10',
-    imageLink: 'Clone_of_Bootstrap_Template.webp',
-    altImage: 'Clone of Bootstrap Template',
-    title: 'Clone of Bootstrap Template',
-    desc: '',
-    date: '02/01/2022'
-},{
-    link:'webpage_9',
-    imageLink: 'Clone_of_Layout_Parser.webp',
-    altImage: 'Clone of Layout Parser',
-    title: 'Clone of Layout Parser',
-    desc: '',
-    date: '01/28/2022'
-},{
-    link:'https://rakshit234.github.io/Portfolio',
-    imageLink: 'Rakshit_Bhaiya_Portfolio.webp',
-    altImage: 'Portfolio',
-    title: 'Portfolio',
-    desc: '',
-    date: '12/01/2021'
-},{
-    link:'webpage_7',
-    imageLink: 'Hulu_Clone_Project.webp',
-    altImage: 'Hulu Clone Project',
-    title: 'Hulu Clone Project',
-    desc: '',
-    date: '10/13/2021'
-},{
-    link:'webpage_6',
-    imageLink: 'HTML5_Massively_Landing_Page.webp',
-    altImage: 'HTML5 Massively Landing Page',
-    title: 'HTML5 Massively Landing Page',
-    desc: '',
-    date: '10/11/2021'
-},{
-    link:'webpage_5',
-    imageLink: 'Eventually_Stunning_HTML5_Landing_Page.webp',
-    altImage: 'Eventually - Stunning HTML5 Landing Page',
-    title: 'Eventually - Stunning HTML5 Landing Page',
-    desc: '',
-    date: '10/09/2021'
-},{
-    link:'webpage_4',
-    imageLink: 'Slack_Clone_Project.webp',
-    altImage: 'Slack Clone Project',
-    title: 'Slack Clone Project',
-    desc: '',
-    date: '10/07/2021'
-},{
-    link:'webpage_3',
-    imageLink: 'Multiverse_HTML5_Photo_Gallery_Project.webp',
-    altImage: 'Multiverse HTML5 Photo Gallery Project',
-    title: 'Multiverse HTML5 Photo Gallery Project',
-    desc: '',
-    date: '10/05/2021'
-},{
-    link:'webpage_12',
-    imageLink: 'Google_Search_Engine.webp',
-    altImage: 'Google Search Engine',
-    title: 'Google Search Engine',
-    desc: 'Google Search Engine using HTML and CSS only.',
-    date: '10/04/2021'
-},{
-    link:'webpage_2',
-    imageLink: 'Editorial_HTML5.webp',
-    altImage: 'Editorial HTML5 Blog Project',
-    title: 'Editorial HTML5 Blog Project',
-    desc: '',
-    date: '10/03/2021'
-},{
-    link:'webpage_1',
-    imageLink: 'Astral_Portfolio.webp',
-    altImage: 'Astral Portfolio Page',
-    title: 'Astral Portfolio Page',
-    desc: '',
-    date: '10/01/2021'
-}]
+const pageData = [
+  {
+    link: "https://first-react-app-rhvsingh.vercel.app/quizapp",
+    imageLink: "Quiz_App.webp",
+    altImage: "Quiz App",
+    title: "Quiz App",
+    desc: "",
+    date: "12/03/2022",
+  },
+  {
+    link: "https://first-react-app-rhvsingh.vercel.app/tenzies",
+    imageLink: "Tenzies_Game.webp",
+    altImage: "Tenzies Game",
+    title: "Tenzies Game",
+    desc: "",
+    date: "11/30/2022",
+  },
+  {
+    link: "https://first-react-app-rhvsingh.vercel.app/notes",
+    imageLink: "Notes_App.webp",
+    altImage: "Notes App",
+    title: "Notes App",
+    desc: "",
+    date: "11/28/2022",
+  },
+  {
+    link: "https://first-react-app-rhvsingh.vercel.app/",
+    imageLink: "Task_Tracker.webp",
+    altImage: "Task Tracker",
+    title: "Task Tracker",
+    desc: "",
+    date: "11/25/2022",
+  },
+  {
+    link: "https://rhvsingh.github.io/intern-putatoe",
+    imageLink: "Intern-Putatoe.webp",
+    altImage: "Intern Putatoe",
+    title: "Intern Putatoe ReactJS",
+    desc: "Clone of Intern Putatoe App Homepage using ReactJS. Highly composible and flexible app.",
+    date: "11/15/2022",
+  },
+  {
+    link: "webpage_11",
+    imageLink: "Celebrare_Assignment.webp",
+    altImage: "Celebrare Assignment",
+    title: "Celebrare Assignment Image",
+    desc: "You can browse and select the image. Select the shape given, and the image will take over that shape and be able to save that image.",
+    date: "11/19/2022",
+  },
+  {
+    link: "webpage_10",
+    imageLink: "Clone_of_Bootstrap_Template.webp",
+    altImage: "Clone of Bootstrap Template",
+    title: "Clone of Bootstrap Template",
+    desc: "This is a clone of template. Created it just for fun and trying transform skew.",
+    date: "02/01/2022",
+  },
+  {
+    link: "webpage_9",
+    imageLink: "Clone_of_Layout_Parser.webp",
+    altImage: "Clone of Layout Parser",
+    title: "Clone of Layout Parser",
+    desc: "It is a clone of the layout parser webpage. I made it as an assignment given by IIT Bombay for Internship.",
+    date: "01/28/2022",
+  },
+  {
+    link: "https://rakshit234.github.io/Portfolio",
+    imageLink: "Rakshit_Bhaiya_Portfolio.webp",
+    altImage: "Portfolio",
+    title: "Portfolio",
+    desc: "About you, education timeline, download resumes,view certificates in full screen, smoother navigation, and a fully responsive portfolio.",
+    date: "12/01/2021",
+  },
+  {
+    link: "webpage_7",
+    imageLink: "Hulu_Clone_Project.webp",
+    altImage: "Hulu Clone Project",
+    title: "Hulu Clone Project",
+    desc: "",
+    date: "10/13/2021",
+  },
+  {
+    link: "webpage_6",
+    imageLink: "HTML5_Massively_Landing_Page.webp",
+    altImage: "HTML5 Massively Landing Page",
+    title: "HTML5 Massively Landing Page",
+    desc: "",
+    date: "10/11/2021",
+  },
+  {
+    link: "webpage_5",
+    imageLink: "Eventually_Stunning_HTML5_Landing_Page.webp",
+    altImage: "Eventually - Stunning HTML5 Landing Page",
+    title: "Eventually - Stunning HTML5 Landing Page",
+    desc: "",
+    date: "10/09/2021",
+  },
+  {
+    link: "webpage_4",
+    imageLink: "Slack_Clone_Project.webp",
+    altImage: "Slack Clone Project",
+    title: "Slack Clone Project",
+    desc: "",
+    date: "10/07/2021",
+  },
+  {
+    link: "webpage_3",
+    imageLink: "Multiverse_HTML5_Photo_Gallery_Project.webp",
+    altImage: "Multiverse HTML5 Photo Gallery Project",
+    title: "Multiverse HTML5 Photo Gallery Project",
+    desc: "",
+    date: "10/05/2021",
+  },
+  {
+    link: "webpage_12",
+    imageLink: "Google_Search_Engine.webp",
+    altImage: "Google Search Engine",
+    title: "Google Search Engine",
+    desc: "Google Search Engine using HTML and CSS only.",
+    date: "10/04/2021",
+  },
+  {
+    link: "webpage_2",
+    imageLink: "Editorial_HTML5.webp",
+    altImage: "Editorial HTML5 Blog Project",
+    title: "Editorial HTML5 Blog Project",
+    desc: "",
+    date: "10/03/2021",
+  },
+  {
+    link: "webpage_1",
+    imageLink: "Astral_Portfolio.webp",
+    altImage: "Astral Portfolio Page",
+    title: "Astral Portfolio Page",
+    desc: "",
+    date: "10/01/2021",
+  },
+]
